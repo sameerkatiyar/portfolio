1b2ceb9ae9c46372f3a76b74eb5b9976f6452cee
Sameer Katiyar <sameerkatiyar2019@gmail.com>
2025-09-20 21:35:53 +0530

    change page name

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
     <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 m-y-20'>
-    About
+    My firt About Page
     </div>      
     </>
   )
