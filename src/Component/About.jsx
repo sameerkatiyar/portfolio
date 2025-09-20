fc8e9f51b06a7a1dccdf006eb1fd4dedeaf57f61
Sameer Katiyar <sameerkatiyar2019@gmail.com>
2025-09-21 00:16:48 +0530

    change page content

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -3,9 +3,46 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 m-y-20'>
-    My firt About Page
-    </div>      
+      <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 m-y-16'>
+        <div>
+          <h1 className='text-3xl font-bold mb-5'>About</h1>
+          <p>Hello, I'm Sameer Katiyar a passionnate web devoloprt with a Keen eye for MERN Stack . With abackgrount in IT, I strive to create empactful and visually stunning Software solution that leave a lasting impression</p>
+          <br />
+          <h1 className='text-green-600 font-semibold text-xl'> Education & Training </h1>
+          <span>
+            [Diploma/Certification], [Institution], [Year], [Degree/Certification], [Year], [Relevent course], [Platform/Institution], [Year]
+          </span>
+          <br />
+          <br />
+          <h1 className='text-green-600 font-semibold text-xl'> Skill & Experties </h1>
+          <span>
+            Proficient in [Programming Language] Experienced with [Software Tools/Technologies] strong grasp of [Design Principles/Concepts] Excellent problem-Solving skill Effective communicator and callaborator
+          </span>
+          <br />
+          <br />
+          <h1 className='text-green-600 font-semibold text-xl'> Professional Experience </h1>
+          <div>
+            <p>
+              Trainee, [RMSI PVT. LTD.], [09/04/2024 to 25/12/2024], [Data anotation]
+            </p>
+            <p>
+              [Data Anotator], [RMSI PVT. LTD.], [24/04/2024 to 25/12/2024], [Data anotation]
+            </p>
+          </div>
+          <br />
+          <br />
+          <h1 className='text-green-600 font-semibold text-xl'> Achievement & Award </h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid nihil dolore quod corrupti maiores impedit vel eveniet, veniam praesentium iste dicta. Ipsa quibusdam, molestias incidunt voluptas non dicta quaerat suscipit.
+          </span>
+           <br />
+          <br />
+          <h1 className='text-green-600 font-semibold text-xl'> Mission Statement </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid nihil dolore quod corrupti maiores impedit vel eveniet, veniam praesentium iste dicta. Ipsa quibusdam, molestias incidunt voluptas non dicta quaerat suscipit.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
